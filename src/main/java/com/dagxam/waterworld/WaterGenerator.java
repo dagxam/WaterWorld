ec9ec62855a692b9ceab359b78f296ed4899ba67
dagxam <dagxam@gmail.com>
2026-08-18 20:15:16 +0300

    Update WaterGenerator.java

--- src/main/java/com/dagxam/waterworld/WaterGenerator.java
+++ src/main/java/com/dagxam/waterworld/WaterGenerator.java
@@ -19,60 +19,59 @@ public class WaterGenerator extends ChunkGenerator {
         int seaLevel = 63; 
         int deepslateLevel = 0; 
 
-        // 1. Генератор для неровного дна (холмы и впадины)
-        SimplexOctaveGenerator terrainGen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed()), 8);
-        terrainGen.setScale(0.008D);
+        // 1. Плавный генератор рельефа дна
+        // Масштаб 0.005D делает холмы очень широкими и гладкими
+        SimplexOctaveGenerator terrainGen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed()), 4);
+        terrainGen.setScale(0.005D);
 
-        // 2. НОВОЕ: Генератор для пещер (Трехмерный шум)
-        SimplexOctaveGenerator caveGen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed()), 4);
-        caveGen.setScale(0.03D); // Масштаб пещер (уменьшите число, чтобы сделать пещеры шире)
+        // 2. Генератор пещер (извилистые подземные тоннели)
+        SimplexOctaveGenerator caveGen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed() + 1), 3);
+        caveGen.setScale(0.015D);
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
                 int realX = chunkX * 16 + x;
                 int realZ = chunkZ * 16 + z;
 
-                // Вычисляем высоту морского дна в этой координате
-                double terrainNoise = terrainGen.noise(realX, realZ, 0.5D, 0.5D);
-                int groundLevel = (int) (30 + (terrainNoise * 15));
+                // Получаем значение шума. Параметр "true" в конце включает идеальное сглаживание
+                double terrainNoise = terrainGen.noise(realX, realZ, 0.5D, 0.5D, true);
+                
+                // Высота морского дна плавно колеблется от 23 до 47
+                int groundLevel = (int) (35 + (terrainNoise * 12));
 
-                // Устанавливаем бедрок в самом низу
+                // Бедрок в самом низу
                 chunkData.setBlock(x, minHeight, z, Material.BEDROCK);
 
-                // Заполняем столб блоков снизу вверх
                 for (int y = minHeight + 1; y <= seaLevel; y++) {
-                    
                     boolean isCave = false;
-                    
-                    // Пещеры могут генерироваться ТОЛЬКО глубоко под землей
-                    // (минимум 4 блока сплошного камня/земли отделяют пещеру от воды)
-                    if (y < groundLevel - 4) {
-                        // Генерируем 3D-шум для текущего блока
-                        double caveNoise = caveGen.noise(realX, y, realZ, 0.5D, 0.5D);
-                        
-                        // Если значение шума выше порога (0.55), создаем пустоту
-                        if (caveNoise > 0.55D) {
+
+                    // Пещеры генерируются ТОЛЬКО если над ними есть минимум 6 блоков прочной породы
+                    if (y < groundLevel - 6) {
+                        double caveNoise = caveGen.noise(realX, y, realZ, 0.5D, 0.5D, true);
+                        if (caveNoise > 0.60D) {
                             isCave = true;
                         }
                     }
 
-                    // Распределяем материалы
-                    if (y <= deepslateLevel) {
-                        // Глубинный сланец (с пещерами)
-                        if (isCave) chunkData.setBlock(x, y, z, Material.CAVE_AIR);
-                        else chunkData.setBlock(x, y, z, Material.DEEPSLATE);
-                    } 
-                    else if (y <= groundLevel - 3) {
-                        // Обычный камень (с пещерами)
-                        if (isCave) chunkData.setBlock(x, y, z, Material.CAVE_AIR);
-                        else chunkData.setBlock(x, y, z, Material.STONE);
-                    } 
-                    else if (y <= groundLevel) {
-                        // Поверхность дна (земля). Всегда сплошная, чтобы вода не утекла в пещеры.
-                        chunkData.setBlock(x, y, z, Material.DIRT);
-                    } 
-                    else if (y <= seaLevel) {
-                        // Вода. Всегда сплошная.
+                    if (y <= groundLevel) {
+                        if (isCave) {
+                            chunkData.setBlock(x, y, z, Material.CAVE_AIR);
+                        } else {
+                            // Распределяем слои пород как в ванильном океане
+                            if (y <= deepslateLevel) {
+                                chunkData.setBlock(x, y, z, Material.DEEPSLATE);
+                            } else if (y < groundLevel - 3) {
+                                chunkData.setBlock(x, y, z, Material.STONE);
+                            } else if (y < groundLevel) {
+                                // Обязательный слой песчаника, чтобы песок не проваливался под землю
+                                chunkData.setBlock(x, y, z, Material.SANDSTONE);
+                            } else {
+                                // Само дно океана теперь состоит из ПЕСКА
+                                chunkData.setBlock(x, y, z, Material.SAND);
+                            }
+                        }
+                    } else {
+                        // Чистая сплошная вода
                         chunkData.setBlock(x, y, z, Material.WATER);
                     }
                 }
@@ -80,12 +79,12 @@ public class WaterGenerator extends ChunkGenerator {
         }
     }
 
-    // Теплый океан для предотвращения спавна льда и айсбергов
     @Override
     public BiomeProvider getDefaultBiomeProvider(WorldInfo worldInfo) {
         return new BiomeProvider() {
             @Override
             public Biome getBiome(WorldInfo worldInfo, int x, int y, int z) {
+                // Теплый океан обеспечивает красивый цвет воды и спавн рыбок
                 return Biome.WARM_OCEAN; 
             }
 
@@ -98,14 +97,12 @@ public class WaterGenerator extends ChunkGenerator {
     
     @Override
     public boolean shouldGenerateDecorations() {
-        // Обязательно true, чтобы Ore-Plugin наполнил камень рудами
-        return true; 
+        return true; // Включает водоросли на песке и работу плагинов на руды
     }
 
     @Override
     public boolean shouldGenerateCaves() {
-        // Обязательно FALSE, ванильные пещеры нам больше не нужны, у нас теперь свои
-        return false; 
+        return false; // Отключаем ломаные ванильные пещеры
     }
 
     @Override
